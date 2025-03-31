ea00de4b5e14763680dc8dccaf823ef1449838b0
SagarSharma950 <156985687+SagarSharma950@users.noreply.github.com>
2025-03-31 22:06:34 +0530

    Time: 0 ms (100%), Space: 21.2 MB (80.49%) - LeetHub

--- 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.cpp
+++ 0950-x-of-a-kind-in-a-deck-of-cards/0950-x-of-a-kind-in-a-deck-of-cards.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int gcd(int a, int b) {
-        if (b == 0) 
-            return a;
-        return gcd(b, a % b);
-    }
     bool hasGroupsSizeX(vector<int>& deck) {
-        unordered_map<int, int> mpp;
-        for (auto it : deck) 
-            mpp[it]++;
-        int ans = 0;
-        for (auto it : mpp) 
-            ans = gcd(it.second, ans);
-        return ans > 1;
+        if (deck.size() < 2) return false;
+        unordered_map<int, int> count;
+        for (int card : deck) {
+            count[card]++;
+        }
+        int gcdValue = 0;
+        for (auto& [_, freq] : count) {
+            gcdValue = gcd(gcdValue, freq);
+        }
+        return gcdValue > 1;
     }
 };
